130c043730090ec3484bdca39c9337ab366d4378
progresso85 <martin.heuripeau@gmail.com>
2020-12-16 15:13:17 +0100

    ajout GaussianBlur

---
+++ filters/blurred.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy
+
+
+def blur_picture():
+    image = cv2.imread('imgs/photo_desert.jpg')
+    image_blur = cv2.GaussianBlur(image,(15,15),0)
+
+    image = cv2.imread('imgs/photo_montagne.jpg')
+    image_blur2 = cv2.GaussianBlur(image, (15, 15), 0)
+
+    image = cv2.imread('imgs/photo_ciel.jpg')
+    image_blur3 = cv2.GaussianBlur(image, (15, 15), 0)
+
+    image = cv2.imread('imgs/photo_ocean.jpg')
+    image_blur4 = cv2.GaussianBlur(image, (15, 15), 0)
+
+    cv2.imwrite('output/Blur/blur_desert.jpg', image_blur)
+    cv2.imwrite('output/Blur/blur_montagne.jpg', image_blur2)
+    cv2.imwrite('output/Blur/blur_ciel.jpg', image_blur3)
+    cv2.imwrite('output/Blur/blur_ocean.jpg', image_blur4)
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from filters import grey
 from filters import distrate
+from filters import blurred
 
 grey.grey_picture()
 
 distrate.distrate_picture()
+
+blurred.blur_picture()
+
