b6d9dc67e139b275f0b936446f27bf76efe093be
Maxime Prioleau <maximeprioleau@gmail.com>
2020-12-17 09:27:32 +0100

    Code propre

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-from filters import grey
-from filters import distension
-from filters import blurred
 import os
 import cv2
-
-
-# grey.grey_picture()
-#
-# distension.distension_picture()
-#
-# blurred.blur_picture()
+from filters import grey, distension, blurred
 
 input_dir = 'imgs'
 files = os.listdir(input_dir)
@@ -17,18 +8,13 @@ print(files)
 
 for f in files:
     print(f)
+
     image = cv2.imread(f'{input_dir}/{f}')
+
     image = grey.grey(image)
-    cv2.imwrite(f'output/{f}', image)
 
-for f in files:
-    print(f)
-    image = cv2.imread(f'{input_dir}/{f}')
     image = distension.distension(image)
-    cv2.imwrite(f'output/{f}', image)
 
-for f in files:
-    print(f)
-    image = cv2.imread(f'{input_dir}/{f}')
     image = blurred.blur(image)
+
     cv2.imwrite(f'output/{f}', image)
