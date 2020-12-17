6fea82936167a09f00f245a0ccccf490fc755da1
progresso85 <martin.heuripeau@gmail.com>
2020-12-17 17:23:00 +0100

    CLI ajout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 from filters import grey, distension, blurred
+import sys
 
 input_dir = 'imgs'
 files = os.listdir(input_dir)
@@ -18,3 +19,46 @@ for f in files:
     image = blurred.blur(image)
 
     cv2.imwrite(f'output/{f}', image)
+
+a = {}
+
+
+args = sys.argv
+print(args)
+for i in range(0, len(args)):
+    arg = args[i]
+    print(i)
+    print(arg)
+    if args[i] == '-i':
+        inputdirectory = args[i + 1]
+    if args[i] == '-o':
+        outputdirectory = args[i + 1]
+
+    if arg == '-h':
+        print("-h,----help")
+        print("-i, --input-dir <directory>")
+        print("-o, --output-dir <directory>")
+
+
+
+files = os.listdir(inputdirectory)
+print(files)
+
+for f in files:
+    print(f)
+
+    image = cv2.imread(inputdirectory + '/' + f)
+
+    image = grey.grey(image)
+
+    image = distension.distension(image)
+
+    image = blurred.blur(image)
+
+    cv2.imwrite(outputdirectory + '/' + f,image)
+
+
+
+
+
+
